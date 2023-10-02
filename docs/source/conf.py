1bf7233615991fdfe45322ee756b618954ff8621
Евгений Кирьян <31926713+EvgenyKiryan@users.noreply.github.com>
2023-10-02 15:08:51 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Renga Style Template Scripting'
 copyright = '2023, Renga Software'
 author = 'Eugene Kiryan'
-release = '2.0'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
